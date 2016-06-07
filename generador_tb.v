3f403a197a0bd236ec239581a6ead2b3f8b7706a
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2016-06-06 22:11:17 -0500

    testbench y módulo de prueba

---
+++ generador_tb.v
@@ -0,0 +1,35 @@
+`timescale 1 ns/100 ps
+
+module generador_tb;
+
+	parameter CLK100_SPEED = 10;  // 100Mhz = 10nS
+	localparam  SZ = 16; // bits of accuracy
+	wire [SZ:0] Xout, Yout;
+	reg CLK_100MHZ = 1'b0;
+	reg [31:0] i = 0;
+	
+	generador uut(.clk(CLK_100MHZ), .Xout(Xout), .Yout(Yout));
+		
+	always @(posedge CLK_100MHZ) begin
+		if (i == 360) begin
+		$write("Simulation has finished");
+		$stop;
+		end
+		i = i+1;
+		end
+		
+		
+
+	initial
+	begin
+	  CLK_100MHZ = 1'b0;
+	  $display ("CLK_100MHZ started");
+	  #5;
+	  forever
+	  begin
+		 #(CLK100_SPEED/2) CLK_100MHZ = 1'b1;
+		 #(CLK100_SPEED/2) CLK_100MHZ = 1'b0;
+	  end
+	end
+
+endmodule
